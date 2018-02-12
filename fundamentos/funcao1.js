25875f78e1734eebb4fb6b4389b93a0c0e43b3f0
RobertoSalcedoAlves <roberto.salcedoalves@gmail.com>
2018-02-12 13:47:23 -0200

    fundamentos - funcoes1

---
+++ fundamentos/funcao1.js
@@ -0,0 +1,21 @@
+//Funcao sem retorno
+function imprimirSoma(a, b){
+    console.log(a + b)
+}
+
+imprimirSoma(2, 3)
+imprimirSoma(2) // SEM ERRO - tenta somar o número indicado com um "undefined" e retorna NaN
+imprimirSoma(2, 3, 4, 5, 6, 7, 8) // SEM ERRO - funciona. Simplesmente ignora o excesso de parâmetros
+imprimirSoma() // SEM ERRO - retorna NaN
+imprimirSoma('vo', 'vó') // Concatena
+imprimirSoma('2', 1) // tenta somar, senão concatena.
+imprimirSoma(7, 'estrelo') // Concatena
+
+// Funcao com retorno
+function soma(a, b = 1){ //um valor padrão é estabelecido para quando não informado
+    return a + b
+}
+
+console.log(soma(2, 3))
+console.log(soma(2))
+console.log(soma()) // ERRO ! - soma de valor "undefined"
---
+++ fundamentos/nullUndefined.js
@@ -0,0 +1,19 @@
+let valor // não inicializada
+console.log(valor)// foi declarada, mas não inicializada. Vai resultar em "undefined"
+
+valor = null // ausência de valor
+console.log(valor) // retorna "null"
+// console.log(valor.toString()) // Erro!
+
+const produto = {}
+console.log(produto.preco)
+console.log(produto)
+
+produto.preco = undefined // evitar atribuir undefined
+console.log(!!produto.preco)
+// delete produto.preco
+console.log(produto)
+
+produto.preco = null // sem preco
+console.log(!!produto.preco)
+console.log(produto)
